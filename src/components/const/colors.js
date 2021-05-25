b3c449955921e8014456e7145fef23ee58f0f555
bpa03 <baldassarepugliese@gmail.com>
2021-05-25 17:00:50 -0400

    Setting the project

---
+++ src/components/const/colors.js
@@ -0,0 +1,10 @@
+/* colors */
+export const primary = "rgba(41, 98, 255, 1);";
+export const secondary = "rgba(69, 90, 100, 1);";
+export const danger = "rgba(211, 47, 47, 1);";
+export const defaultColor = "rgba(224, 224, 224, 1);";
+
+/* Hover Colors */
+export const primaryHover = "rgba(0, 57, 203, 1);";
+export const secondaryHover = "rgba(28, 49, 58, 1);";
+export const dangerHover = "rgba(154, 0, 7, 1);";
