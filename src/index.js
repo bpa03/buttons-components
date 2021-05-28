49a842204a0137ca70c876dbf5c4d5d02935f63a
bpa03 <baldassarepugliese@gmail.com>
2021-05-28 00:16:11 -0400

    Material-icons added to project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 
 import './components/styles/index.css';
+import '../node_modules/material-icons/iconfont/material-icons.css';
 
 ReactDOM.render(
   <React.StrictMode>
